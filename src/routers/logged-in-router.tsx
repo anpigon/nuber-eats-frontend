34338d90d62da4112ed90442169596e90a5075b2
anpigon <markan82@gmail.com>
2020-12-13 16:20:40 +0900

    #15.15 Using the Token

--- src/routers/logged-in-router.tsx
+++ src/routers/logged-in-router.tsx
@@ -1,10 +1,34 @@
+import { gql, useQuery } from "@apollo/client";
 import React from "react";
 import { isLoggedInVar } from "../apollo";
 
+const ME_QUERY = gql`
+  query meQuery {
+    me {
+      id
+      email
+      role
+      verified
+    }
+  }
+`;
+
 export const LoggedInRouter = () => {
+  const { data, loading, error } = useQuery(ME_QUERY);
+  console.log(data, error);
+
   const onClick = () => {
     isLoggedInVar(false);
   };
+
+  if (loading) {
+    return (
+      <div className="h-screen flex justify-center items-center">
+        <span className="font-medium text-xl">Loading...</span>
+      </div>
+    );
+  }
+
   return (
     <div>
       <h1>Logged In</h1>
