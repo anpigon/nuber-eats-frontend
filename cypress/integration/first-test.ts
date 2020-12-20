f86f055fac9f7e5d451ec2842350ce16680b4403
anpigon <markan82@gmail.com>
2020-12-20 13:59:58 +0900

    #19.2 Login E2E

--- cypress/integration/first-test.ts
+++ cypress/integration/first-test.ts
@@ -1,23 +1,33 @@
 describe("Log In", () => {
-  it("should see login page", () => {
-    cy.visit("/").title().should("eq", "Login | Nuber Eats");
-  });
+  const user = cy;
 
-  it("can fill out the form", () => {
-    cy.visit("/")
-      .get("[name=email")
-      .type("nico@nomadcoders.co")
-      .get("[name=password")
-      .type("123456")
-      .get("button")
-      .should("not.have.class", "pointer-events-none");
+  it("should see login page", () => {
+    user.visit("/").title().should("eq", "Login | Nuber Eats");
   });
 
   it("can see email / password validation errors", () => {
-    cy.visit("/")
-      .get("[name=email")
-      .type("bad@email")
-      .get("[role=alert]")
-      .should("have.text", "Please enter a valid email");
+    user.visit("/");
+    user.findByPlaceholderText(/email/i).type("bad@email");
+    user.findByRole("alert").should("have.text", "Please enter a valid email");
+    user.findByPlaceholderText(/email/i).clear();
+    user.findByRole("alert").should("have.text", "Email is required");
+    user.findByPlaceholderText(/email/i).type("bad@email.com");
+    user
+      .findByPlaceholderText(/password/i)
+      .type("a")
+      .clear();
+    user.findByRole("alert").should("have.text", "Password is required");
+  });
+
+  it("can fill out the form", () => {
+    user.visit("/");
+    user.get("[name=email").type("anpigon@gmail.com");
+    user.get("[name=password").type("111111");
+    user.get("button").should("not.have.class", "pointer-events-none").click();
+    user.window().its("localStorage.nuber-token").should("be.a", "string");
+  });
+
+  it("sign up", () => {
+    user.visit("/create-account");
   });
 });
