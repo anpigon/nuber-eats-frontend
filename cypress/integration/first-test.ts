c6a7b0aba59a6ecec1f31fc0f53c6b14b19a4c33
anpigon <markan82@gmail.com>
2020-12-20 13:48:42 +0900

    #19.1 Our First Cypress Test

--- cypress/integration/first-test.ts
+++ cypress/integration/first-test.ts
@@ -1,7 +1,23 @@
-describe("First Test", () => {
-  it("should go to homepage", () => {
-    cy.visit("http://localhost:3000")
-      .title()
-      .should("eq", "Login | Nuber Eats");
+describe("Log In", () => {
+  it("should see login page", () => {
+    cy.visit("/").title().should("eq", "Login | Nuber Eats");
+  });
+
+  it("can fill out the form", () => {
+    cy.visit("/")
+      .get("[name=email")
+      .type("nico@nomadcoders.co")
+      .get("[name=password")
+      .type("123456")
+      .get("button")
+      .should("not.have.class", "pointer-events-none");
+  });
+
+  it("can see email / password validation errors", () => {
+    cy.visit("/")
+      .get("[name=email")
+      .type("bad@email")
+      .get("[role=alert]")
+      .should("have.text", "Please enter a valid email");
   });
 });
